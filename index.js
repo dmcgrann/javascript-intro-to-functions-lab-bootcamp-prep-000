efc328e954418081f2ac899f3a6c7eb0a36fa5d8
David McGrann <dpmcgrann@gmail.com>
2017-10-22 19:43:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function logShout() {
 function logWhisper() {
   console.log('hello')
 }
-function sayHiToGrandma() {
+function sayHiToGrandma(string) {
   return string.toLowerCase("I can\'t hear you!")
 }
